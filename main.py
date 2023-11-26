a091701fd36ff6d376215441ee36790d87fbb9f8
Joel Dsouza <shadowk97@gmail.com>
2023-11-26 23:08:54 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import discord
+import os
+from googletrans import Translator
+from dotenv.main import load_dotenv
+from languages import languages
+
+# Intents
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+# Connecting to Client
+@client.event
+async def on_ready():
+    print(f"We logged in as {client.user}")
+
+@client.event
+async def on_reaction_add(reaction, user):
+    if reaction.emoji:
+        reactedEmoji = reaction.emoji
+        if not(languages.get(reactedEmoji)):
+            await reaction.message.channel.send("Language not added!")
+        else:
+            selectedLanguage = languages.get(reactedEmoji)
+            sentMessage = str(reaction.message.content)
+            translator = Translator()
+            sendMessage = translator.translate(sentMessage, dest=selectedLanguage)
+            await reaction.message.channel.send(sendMessage.text)
+
+load_dotenv()
+token = os.getenv("TOKEN")
+client.run(token)

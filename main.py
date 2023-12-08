f6ad0c7903f0d675f060474a10ac03427645090d
Joel Dsouza <shadowk97@gmail.com>
2023-12-08 15:21:30 +0530

    Modified function to work for  reactions on both old and new messages.

--- main.py
+++ main.py
@@ -15,17 +15,22 @@ async def on_ready():
     print(f"We logged in as {client.user}")
 
 @client.event
-async def on_reaction_add(reaction, user):
+async def on_raw_reaction_add(reaction):
+    channel = await client.fetch_channel(reaction.channel_id)
+    message = await channel.fetch_message(reaction.message_id)
+    userid = reaction.user_id
+
     if reaction.emoji:
-        reactedEmoji = reaction.emoji
-        if not(languages.get(reactedEmoji)): 
+        reactedEmoji = reaction.emoji.name
+        if not(languages.get(reactedEmoji)):
+            print(f"{reactedEmoji} Language not found")
             pass
         else:
             selectedLanguage = languages.get(reactedEmoji)
-            sentMessage = str(reaction.message.content)
+            sentMessage = str(message.content)
             translator = Translator()
             sendMessage = translator.translate(sentMessage, dest=selectedLanguage)
-            await reaction.message.reply(f"<@{user.id}> {reactedEmoji} Translation:\n\n"+sendMessage.text)
+            await channel.send(f"<@{userid}> {reactedEmoji} Translation:\n\n"+sendMessage.text)
 
 load_dotenv()
 token = os.getenv("TOKEN")
